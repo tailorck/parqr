dceff620f09b176a2714a5db0c28d9a2f7030eec
tailorck <cktailor34@gmail.com>
2017-10-19 21:09:18 -0400

    modified scraper to update old posts with any changes in fields and modified Post model to allow pretty print

--- app/models.py
+++ app/models.py
@@ -2,8 +2,8 @@ from app import db
 
 
 class Followup(db.EmbeddedDocument):
-    followup = db.StringField(required=True)
-    feedback = db.ListField(db.StringField())
+    text = db.StringField(required=True)
+    responses = db.ListField(db.StringField())
 
 
 class Post(db.Document):
@@ -16,22 +16,22 @@ class Post(db.Document):
     i_answer = db.StringField()
     followups = db.ListField(db.EmbeddedDocumentField(Followup))
 
-    def __str__(self):
-        return '<{}: id={!r}>'.format(type(self).__name__, self.id)
-
-    def __repr__(self):
+    def pprint(self):
         def _format_long_string(string):
             string = string.encode('ascii', 'ignore')
             string = string.replace('\n', ' ')
             string = ' '.join(string.split(' ')[:6]) + '...'
+            return string
 
         attrs = []
-        for name in self._fields.keys():
+        print '<{}: id={!r}>'.format(type(self).__name__, self.id)
+        fields = ['cid', 'pid', 'subject', 'body', 'tags', 's_answer',
+                  'i_answer', 'followups']
+        for name in fields:
             value = getattr(self, name)
             if isinstance(value, unicode):
                 value = _format_long_string(value)
-            attrs.append('\n    {} = {},'.format(name, value))
-        return '<{}: {}\n>'.format(type(self).__name__, ''.join(attrs))
+            print '    {} = {}'.format(name, value)
 
 
 class Course(db.Document):
--- app/scraper.py
+++ app/scraper.py
@@ -1,6 +1,7 @@
 import re
 
 from threading import Thread
+import time
 import logging
 
 from bs4 import BeautifulSoup
@@ -10,7 +11,7 @@ from progressbar import ProgressBar
 
 from .exception import InvalidUsage
 from .models import Course, Post
-from .utils import read_credentials
+from .utils import read_credentials, stringify_followups
 
 
 class Scraper():
@@ -32,8 +33,8 @@ class Scraper():
 
         self._login()
 
-    def pull_new_posts(self, course_id):
-        """Creates a thread task to retrieve all new posts for a course
+    def update_posts(self, course_id):
+        """Creates a thread task to update all posts in a course
 
         Parameters
         ----------
@@ -45,13 +46,14 @@ class Scraper():
 
         # TODO: Catch invalid course_id exception
         network = self._piazza.network(course_id)
-        self._threads[course_id] = Thread(target=self._pull_new_posts,
+        self._threads[course_id] = Thread(target=self._update_posts,
                                           args=(course_id, network,))
 
         self._threads[course_id].start()
 
-    def _pull_new_posts(self, course_id, network):
+    def _update_posts(self, course_id, network):
         """Retrieves all new posts in course that are not already in database
+        and updates old posts that have been modified
 
         Parameters
         ----------
@@ -71,11 +73,8 @@ class Scraper():
         if not course:
             course = Course(course_id).save()
 
+        start_time = time.time()
         for pid in pbar(xrange(1, total_questions+1)):
-            # skip this post if it is already in the database
-            if Post.objects(cid=course_id, pid=pid):
-                continue
-
             # Get the post if available
             try:
                 post = network.get_post(pid)
@@ -95,12 +94,59 @@ class Scraper():
             # Extract the followups and feedbacks if applicable
             followups = self._extract_followups(post)
 
-            # Create a new post and add it to the course
-            mongo_post = Post(course_id, pid, subject, body, tags, s_answer,
-                              i_answer, followups).save()
-            course.update(add_to_set__posts=mongo_post)
+            # If post exists, check if it has been updated and update db if
+            # necessary. Else, insert new post and add to course's post list
+            if Post.objects(cid=course_id, pid=pid):
+                db_post = Post.objects.get(cid=course_id, pid=pid)
+                new_fields = dict(subject=subject, body=body,
+                                  s_answer=s_answer, i_answer=i_answer,
+                                  followups=followups)
+                is_updated = self._check_for_updates(db_post, new_fields)
 
+                if is_updated is True:
+                    db_post.update(subject=subject, body=body,
+                                   s_answer=s_answer, i_answer=i_answer,
+                                   followups=followups)
+            else:
+                mongo_post = Post(course_id, pid, subject, body, tags,
+                                  s_answer, i_answer, followups).save()
+                course.update(add_to_set__posts=mongo_post)
+
+        end_time = time.time()
+        time_elapsed = end_time - start_time
+        self._logger.info('Course updated in: {:.2f}s'.format(time_elapsed))
         self._threads.pop(course_id)
+
+    def _check_for_updates(self, curr_post, new_fields):
+        """Checks if post has been updated since last scrape.
+
+        Parameters
+        ----------
+        curr_post : app.models.Post
+            The current post in the database.
+        new_fields : dict
+            A dictionary of the most recent scraped fields for the given post
+
+        Returns
+        -------
+        is_updated : boolean
+            True if any of the fields have been changed. Otherwise, False.
+        """
+        curr_followups_str = stringify_followups(curr_post.followups)
+        new_followups_str = stringify_followups(new_fields['followups'])
+
+        if curr_post.subject != new_fields['subject']:
+            return True
+        elif curr_post.body != new_fields['body']:
+            return True
+        elif curr_post.s_answer != new_fields['s_answer']:
+            return True
+        elif curr_post.i_answer != new_fields['i_answer']:
+            return True
+        elif curr_followups_str != new_followups_str:
+            return True
+
+        return False
 
     def _extract_question_details(self, post):
         """Retrieves information pertaining to the question in the piazza post
@@ -113,12 +159,12 @@ class Scraper():
 
         Returns
         -------
-            subject : str
-                The subject of the piazza post
-            parsed_body : str
-                The body of the post without html tags
-            tags : list
-                A list of the tags or folders that the post belonged to
+        subject : str
+            The subject of the piazza post
+        parsed_body : str
+            The body of the post without html tags
+        tags : list
+            A list of the tags or folders that the post belonged to
         """
         subject = post['history'][0]['subject']
         html_body = post['history'][0]['content']
@@ -170,22 +216,22 @@ class Scraper():
             contain feedbacks as well (Default = []).
         """
         followups = []
-        for response in post['children']:
+        for child in post['children']:
 
             data = {}
-            if response['type'] == 'followup':
-                html_text = response['subject']
+            if child['type'] == 'followup':
+                html_text = child['subject']
                 soup = BeautifulSoup(html_text, 'html.parser')
-                data['followup'] = soup.get_text()
+                data['text'] = soup.get_text()
 
-                feedback = []
-                if response['children']:
-                    for activity in response['children']:
+                responses = []
+                if child['children']:
+                    for activity in child['children']:
                         html_text = activity['subject']
                         soup = BeautifulSoup(html_text, 'html.parser')
-                        feedback.append(soup.get_text())
+                        responses.append(soup.get_text())
 
-                data['feedback'] = feedback
+                data['responses'] = responses
 
                 followups.append(data)
 
