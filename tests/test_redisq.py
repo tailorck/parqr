7c41dc5a9b7eed754322910dc9d859ba16070cd4
Girish Murali <gmurali6@gatech.edu>
2018-02-22 23:14:41 -0500

    Made code formatting changes

--- tests/test_redisq.py
+++ tests/test_redisq.py
@@ -16,7 +16,7 @@ from rq_scheduler import Scheduler
 
 endpoint = '/api/class'
 course_id = 'j65vl23t'
-payload = dict(cid=course_id)#
+payload = dict(cid=course_id)
 
 
 @mock.patch('app.api.redis')
@@ -25,7 +25,7 @@ def test_redis_exists(mock_redis, client):
     # case 1: cid does not exist, register cid
     mock_redis.exists.return_value = False
     client.post(endpoint, data=json.dumps(payload),
-                    content_type='application/json')
+                content_type='application/json')
     assert mock_redis.exists.called == True, \
         "Failed to call redis.exists(cid) in register_class"
     mock_redis.exists.assert_called_with(course_id)
@@ -42,7 +42,7 @@ def test_redis_exists(mock_redis, client):
     # case 3: cid exists, deregister cid
     mock_redis.exists.return_value = True
     client.delete(endpoint, data=json.dumps(payload),
-                content_type='application/json')
+                  content_type='application/json')
     assert mock_redis.exists.called == True, \
         "Failed to call redis.exists(cid) in deregister_class"
     mock_redis.exists.assert_called_with(course_id)
@@ -62,7 +62,7 @@ def test_redis_get(mock_redis, client):
     # case 1: cid does not exist, cannot deregister cid
     mock_redis.exists.return_value = False
     client.delete(endpoint, data=json.dumps(payload),
-                 content_type='application/json')
+                  content_type='application/json')
     assert mock_redis.get.called == False, \
         "Failed to not call redis.get(cid) in deregister_class"
     mock_redis.reset_mock()
@@ -122,7 +122,7 @@ def test_scheduler_schedule(mock_redis, mock_scheduler, client):
     # case 1: cid does not exist, register cid
     mock_redis.exists.return_value = False
     client.post(endpoint, data=json.dumps(payload),
-                  content_type='application/json')
+                content_type='application/json')
     assert mock_scheduler.schedule.called == True, \
         "Failed to call scheduler.schedule in register_class"
     mock_redis.reset_mock()
@@ -131,7 +131,7 @@ def test_scheduler_schedule(mock_redis, mock_scheduler, client):
     # case 2: cid exists, cannot register cid
     mock_redis.exists.return_value = True
     client.post(endpoint, data=json.dumps(payload),
-                  content_type='application/json')
+                content_type='application/json')
     assert mock_scheduler.schedule.called == False, \
         "Failed to not call scheduler.schedule in register_class"
 
@@ -155,23 +155,3 @@ def test_scheduler_get_jobs(mock_redis, mock_scheduler, client):
                   content_type='application/json')
     assert mock_scheduler.get_jobs.called == False, \
         "Failed to not call scheduler.get_jobs in register_class"
-
-# @mock.patch('app.api.scheduler')
-# @mock.patch('app.api.redis')
-# def test_scheduler_cancel(mock_redis, mock_scheduler, client):
-#
-#     # case 1: cid exists, deregister cid
-#     mock_redis.exists.return_value = True
-#     client.delete(endpoint, data=json.dumps(payload),
-#                   content_type='application/json')
-#     assert mock_scheduler.cancel.called == True, \
-#         "Failed to call scheduler.cancel in deregister_class"
-#     mock_redis.reset_mock()
-#     mock_scheduler.reset_mock()
-#
-#     # case 2: cid does not exist, cannot deregister cid
-#     mock_redis.exists.return_value = False
-#     client.delete(endpoint, data=json.dumps(payload),
-#                   content_type='application/json')
-#     assert mock_scheduler.cancel.called == False, \
-#         "Failed to not call scheduler.cancel in register_class"
