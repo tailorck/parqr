b49a8e941bc6b0239f1096b4a4e09a0cd30eb04e
tailorck <cktailor34@gmail.com>
2020-04-28 18:08:25 -0400

    try make_response

--- app/resources/dashboard.py
+++ app/resources/dashboard.py
@@ -1,14 +1,16 @@
 import requests
 import json
 from flask_restful import Resource
-from flask import render_template
+from flask import render_template, make_response
 
 class Dashboard(Resource):
-    
+
     def _init_(self):
         pass
 
     def get(self):
         api_result=requests.get('https://aws.parqr.io/dev/courses')
         course_info=json.loads(api_result.text)
-        return render_template('parqr.html', course_info=course_info, len=len(course_info))
+        headers = {"Content-Type": "text/html"}
+        return make_response(render_template('parqr.html', course_info=course_info, len=len(course_info)),
+                             200, headers)
