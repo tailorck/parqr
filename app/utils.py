b008e3f5758e33f2876da06421de1a02dd79e7fe
Aadarsh Padiyath <aspadiyath@gmail.com>
2020-05-27 17:45:45 -0500

    Add methods for Instructor Dashboard (#180)
    * Change Feedback min and max ratings
    
    * Change feedback request percentage
    
    * Add assignee and resolve methods
    
    * BLACK

--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,5 @@
 from flask import Flask, request
 
-from app.config import config_dict
 from app.exception import InvalidUsage
 
 
@@ -18,10 +17,43 @@ def create_app(app_name):
 
 def verify_non_empty_json_request(func):
     def wrapper(*args, **kwargs):
-        if request.get_data() == '':
-            raise InvalidUsage('No request body provided', 400)
+        if request.get_data() == "":
+            raise InvalidUsage("No request body provided", 400)
         if not request.json:
-            raise InvalidUsage('Request body must be in JSON format', 400)
+            raise InvalidUsage("Request body must be in JSON format", 400)
         return func(*args, **kwargs)
+
     wrapper.__name__ = func.__name__
     return wrapper
+
+
+def pretty_date(time=False):
+    """
+    Get a datetime object or a int() Epoch timestamp and return a
+    pretty string like 'an hour ago', 'Yesterday', '3 months ago',
+    'just now', etc
+    """
+    from datetime import datetime
+
+    now = datetime.now()
+    if isinstance(time, int):
+        diff = now - datetime.fromtimestamp(time)
+    elif isinstance(time, datetime):
+        diff = now - time
+    elif not time:
+        diff = now - now
+    day_diff = diff.days
+
+    if day_diff < 0:
+        return ""
+    if day_diff == 0:
+        return "today"
+    if day_diff == 1:
+        return "yesterday"
+    if day_diff < 7:
+        return str(day_diff) + "d ago"
+    if day_diff < 31:
+        return str(day_diff / 7) + "w ago"
+    if day_diff < 365:
+        return str(day_diff / 30) + "mon ago"
+    return str(day_diff / 365) + "y ago"
